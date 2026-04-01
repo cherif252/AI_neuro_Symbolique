5348af98bdb065759f7d0b4115d99ce2fa85b32a
cherif252 <cherifhibrahim19@gmail.com>
2026-04-01 09:47:23 +0000

    Add neural_network.py implementation

---
+++ neural_network.py
@@ -0,0 +1,33 @@
+"""
+Neural Network Implementation for Neuro-Symbolic AI
+"""
+
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self, input_size, hidden_size, output_size):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        
+        # Initialize weights
+        self.W1 = np.random.randn(input_size, hidden_size) * 0.01
+        self.b1 = np.zeros((1, hidden_size))
+        self.W2 = np.random.randn(hidden_size, output_size) * 0.01
+        self.b2 = np.zeros((1, output_size))
+    
+    def forward(self, X):
+        self.z1 = np.dot(X, self.W1) + self.b1
+        self.a1 = np.relu(self.z1)
+        self.z2 = np.dot(self.a1, self.W2) + self.b2
+        self.a2 = self.softmax(self.z2)
+        return self.a2
+    
+    @staticmethod
+    def relu(x):
+        return np.maximum(0, x)
+    
+    @staticmethod
+    def softmax(x):
+        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
